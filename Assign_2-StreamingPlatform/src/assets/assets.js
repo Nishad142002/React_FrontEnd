11a48df8e32bbe8803a9ca5278ed928f1ebedf28
Nishad_Sheikh <nishad131310@gmail.com>
2025-12-27 18:45:22 +0530

    again fixing path for images

--- Assign_2-StreamingPlatform/src/assets/assets.js
+++ Assign_2-StreamingPlatform/src/assets/assets.js
@@ -1,35 +1,35 @@
-export const pic = "public/profile.jpg";
+export const pic = `${import.meta.env.BASE_URL}/profile.jpg`;
 
-export const slider1 = "public/SliderImg/hero1.jpg";
+export const slider1 = `${import.meta.env.BASE_URL}/SliderImg/hero1.jpg`;
 
 export const continueWatching = [
   {
     name: "The Batman",
-    img: "public/continue/batman.jpg",
+    img: `${import.meta.env.BASE_URL}/continue/batman.jpg`,
     year: "2021",
     duration: "2hr 12min",
   },
   {
     name: "Moon Knight S1",
-    img: "public/continue/moonKnight.jpeg",
+    img: `${import.meta.env.BASE_URL}/continue/moonKnight.jpeg`,
     year: "2024",
     duration: "Season 1",
   },
   {
     name: "Venom",
-    img: "public/continue/venom.jpeg",
+    img: `${import.meta.env.BASE_URL}/continue/venom.jpeg`,
     year: "2019",
     duration: "1hr 45min",
   },
   {
     name: "Loki S2",
-    img: "public/continue/loki.jpeg",
+    img: `${import.meta.env.BASE_URL}/continue/loki.jpeg`,
     year: "2024",
     duration: "Season 2",
   },
   {
     name: "Transformer 3",
-    img: "public/continue/img1.jpeg",
+    img: `${import.meta.env.BASE_URL}/continue/img1.jpeg`,
     year: "2015",
     duration: "2hr 18min",
   },
