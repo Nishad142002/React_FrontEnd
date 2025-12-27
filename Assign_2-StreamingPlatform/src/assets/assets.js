890efa44a4ae544e34295bef3d89a49d2fed7d92
Nishad_Sheikh <nishad131310@gmail.com>
2025-12-27 18:50:08 +0530

    agiain upadated the path

--- Assign_2-StreamingPlatform/src/assets/assets.js
+++ Assign_2-StreamingPlatform/src/assets/assets.js
@@ -1,35 +1,35 @@
-export const pic = `${import.meta.env.BASE_URL}/profile.jpg`;
+export const pic = `${import.meta.env.BASE_URL}profile.jpg`;
 
-export const slider1 = `${import.meta.env.BASE_URL}/SliderImg/hero1.jpg`;
+export const slider1 = `${import.meta.env.BASE_URL}SliderImg/hero1.jpg`;
 
 export const continueWatching = [
   {
     name: "The Batman",
-    img: `${import.meta.env.BASE_URL}/continue/batman.jpg`,
+    img: `${import.meta.env.BASE_URL}continue/batman.jpg`,
     year: "2021",
     duration: "2hr 12min",
   },
   {
     name: "Moon Knight S1",
-    img: `${import.meta.env.BASE_URL}/continue/moonKnight.jpeg`,
+    img: `${import.meta.env.BASE_URL}continue/moonKnight.jpeg`,
     year: "2024",
     duration: "Season 1",
   },
   {
     name: "Venom",
-    img: `${import.meta.env.BASE_URL}/continue/venom.jpeg`,
+    img: `${import.meta.env.BASE_URL}continue/venom.jpeg`,
     year: "2019",
     duration: "1hr 45min",
   },
   {
     name: "Loki S2",
-    img: `${import.meta.env.BASE_URL}/continue/loki.jpeg`,
+    img: `${import.meta.env.BASE_URL}continue/loki.jpeg`,
     year: "2024",
     duration: "Season 2",
   },
   {
     name: "Transformer 3",
-    img: `${import.meta.env.BASE_URL}/continue/img1.jpeg`,
+    img: `${import.meta.env.BASE_URL}continue/img1.jpeg`,
     year: "2015",
     duration: "2hr 18min",
   },
